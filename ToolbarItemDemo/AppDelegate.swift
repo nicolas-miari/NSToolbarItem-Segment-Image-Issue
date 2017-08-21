4f0b66c225984816868fc222e40ef17eb1bf666f
Nicolás Miari <nicolasmiari@gmail.com>
2017-08-21 17:34:31 +0900

    Add working project

---
+++ ToolbarItemDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToolbarItemDemo
+//
+//  Created by Nicolás Miari on 2017/08/21.
+//  Copyright © 2017 Nicolás Miari. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ToolbarItemDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToolbarItemDemo
+//
+//  Created by Nicolás Miari on 2017/08/21.
+//  Copyright © 2017 Nicolás Miari. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
